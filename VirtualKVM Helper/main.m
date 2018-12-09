273160d193f03f7e9a00b0fde1da86a890961456
Soneé John <sonee@alphasoftware.co>
2018-12-09 11:06:50 -0400

    Add new helper target

---
+++ VirtualKVM Helper/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  VirtualKVM Helper
+//
+//  Created by Soneé John on 12/9/18.
+//  Copyright © 2018 Fast Wombat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
