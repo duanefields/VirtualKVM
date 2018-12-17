8bbd759c2015ac479775f5007a2a79e905a00fba
Soneé John <sonee@alphasoftware.co>
2018-12-17 10:26:51 -0400

    Add initial setup for helper tool

---
+++ VirtualKVMHelper/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  VirtualKVMHelper
+//
+//  Created by Soneé John on 17/12/2018.
+//  Copyright © 2018 Fast Wombat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
