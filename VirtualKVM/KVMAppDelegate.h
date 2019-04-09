71f6547ad26689d8031165cb034d39fc221ef7ac
Soneé John <sonee@alphasoftware.co>
2019-04-09 19:04:45 -0400

    Add helper tool

--- VirtualKVM/KVMAppDelegate.h
+++
@@ -1,5 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-@interface KVMAppDelegate : NSObject <NSApplicationDelegate>
-
-@end
---
+++ VirtualKVMHelper/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VirtualKVMHelper
+//
+//  Created by Soneé John on 4/9/19.
+//  Copyright © 2019 Fast Wombat. All rights reserved.
+//
+
+import Foundation
+
+let helper = Helper()
+helper.run()
